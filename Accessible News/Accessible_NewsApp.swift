69c4858a2d8c9a9a79446fcac8430ca66ef6996b
GujMeister <90295019+GujMeister@users.noreply.github.com>
2024-06-16 21:57:08 +0400

    Initial Commit

---
+++ Accessible News/Accessible_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessible_NewsApp.swift
+//  Accessible News
+//
+//  Created by Luka Gujejiani on 16.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Accessible_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessible News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Accessible News
+//
+//  Created by Luka Gujejiani on 16.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
